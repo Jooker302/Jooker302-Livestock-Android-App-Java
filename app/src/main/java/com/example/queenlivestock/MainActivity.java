e13707d149f2b5f5d99be1954cd1b73d47cbeda1
Jooker302 <i.omer6637@gmail.com>
2023-05-14 23:58:25 +0500

    Main page Almost Done

--- app/src/main/java/com/example/queenlivestock/MainActivity.java
+++ app/src/main/java/com/example/queenlivestock/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.queenlivestock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.next_page_button);
+        imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext() ,Login.class);
+                    startActivity(i);
+                }
+            });
+
     }
+
 }
