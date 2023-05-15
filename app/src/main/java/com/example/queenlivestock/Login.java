4bd0e82af1e5dd8f092b5c5c022b76dc5daea106
Jooker302 <i.omer6637@gmail.com>
2023-05-15 22:56:33 +0500

    LoginPage Registernow design done

--- app/src/main/java/com/example/queenlivestock/Login.java
+++ app/src/main/java/com/example/queenlivestock/Login.java
@@ -3,6 +3,9 @@ package com.example.queenlivestock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView textView = findViewById(R.id.registernowlink);
+
+        String text = getString(R.string.registernow);
+        SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new UnderlineSpan(), 0, text.length(), 0);
+
+        textView.setText(spannableString);
+
     }
 }
