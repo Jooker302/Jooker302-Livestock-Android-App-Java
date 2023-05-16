99499adac9ca1976301279f2ca65eab7fe3feddc
Jooker302 <i.omer6637@gmail.com>
2023-05-16 22:39:29 +0500

    Register page Java Done

--- app/src/main/java/com/example/queenlivestock/Register.java
+++ app/src/main/java/com/example/queenlivestock/Register.java
@@ -2,7 +2,12 @@ package com.example.queenlivestock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView loginlink = findViewById(R.id.loginlink);
+
+        String text = getString(R.string.registernow);
+        SpannableString spannableString = new SpannableString(text);
+        spannableString.setSpan(new UnderlineSpan(), 0, text.length(), 0);
+
+        loginlink.setText(spannableString);
+
+        loginlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Login.class);
+                startActivity(i);
+            }
+        });
     }
 }
