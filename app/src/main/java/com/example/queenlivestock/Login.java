66cc1d7ab255d7feec381337665ecd9648081887
Jooker302 <i.omer6637@gmail.com>
2023-05-16 22:45:11 +0500

    Login on back applied

--- app/src/main/java/com/example/queenlivestock/Login.java
+++ app/src/main/java/com/example/queenlivestock/Login.java
@@ -8,9 +8,12 @@ import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    private static final int TIME_DELAY = 2000;
+    private static long back_pressed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +37,16 @@ public class Login extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        if (back_pressed + TIME_DELAY > System.currentTimeMillis()) {
+            finishAffinity();
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        back_pressed = System.currentTimeMillis();
     }
 }
