e090f94b13ca2965e50fd594c6347bd8ea1f6908
Jooker302 <i.omer6637@gmail.com>
2023-05-24 21:08:49 +0500

    Register Page Image Uploader

--- app/src/main/java/com/example/queenlivestock/Register.java
+++ app/src/main/java/com/example/queenlivestock/Register.java
@@ -1,22 +1,31 @@
 package com.example.queenlivestock;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
-
+    private static final int PICK_IMAGE_REQUEST_CODE = 1;
+    private ActivityResultLauncher<Intent> imagePickerLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
         TextView loginlink = findViewById(R.id.loginlink);
+        ImageView register_user_image = findViewById(R.id.register_user_image);
 
         String text = getString(R.string.login_now);
         SpannableString spannableString = new SpannableString(text);
@@ -29,6 +38,31 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),Login.class);
                 startActivity(i);
+            }
+        });
+
+        imagePickerLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Intent data = result.getData();
+
+                        // Handle the selected image data here
+                        Uri selectedImageUri = data.getData();
+                        String imageUriString = selectedImageUri.toString();
+//                        Toast.makeText(getApplicationContext(),imageUriString,Toast.LENGTH_LONG).show();
+                        ImageView imageView = findViewById(R.id.register_user_image);
+                        imageView.setImageURI(selectedImageUri);
+                        imageView.setClipToOutline(true);
+                        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    }
+                }
+        );
+        register_user_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                imagePickerLauncher.launch(intent);
             }
         });
     }
