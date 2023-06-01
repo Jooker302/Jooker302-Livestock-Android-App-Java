b5239e37756d154dd04031a42f78bf3a1879aed6
Jooker302 <i.omer6637@gmail.com>
2023-06-01 14:38:48 +0500

    Color Scheme Appled

--- app/src/main/java/com/example/queenlivestock/PostAdapter.java
+++ app/src/main/java/com/example/queenlivestock/PostAdapter.java
@@ -1,13 +1,18 @@
 package com.example.queenlivestock;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
@@ -43,6 +48,7 @@ public class PostAdapter extends ArrayAdapter<PostClass> {
         }
         PostClass post = posts.get(position);
 
+        LinearLayout post_inner_box = convertView.findViewById(R.id.post_inner_box);
         ImageView imageView = convertView.findViewById(R.id.post_image);
         TextView nameTextView = convertView.findViewById(R.id.post_name);
         TextView priceTextView = convertView.findViewById(R.id.post_price);
@@ -51,6 +57,15 @@ public class PostAdapter extends ArrayAdapter<PostClass> {
         nameTextView.setText(post.getTitle());
         priceTextView.setText("Rs " + post.getPrice());
 
+        int colorResource = R.color.de_active;
+        Drawable backgroundDrawable = ContextCompat.getDrawable(context, colorResource);
+
+
+        if(post.getActive().trim().matches("1")){
+
+        }else{
+            post_inner_box.setBackground(backgroundDrawable);
+        }
         // Load the image using Glide
         RequestOptions requestOptions = new RequestOptions()
                 .diskCacheStrategy(DiskCacheStrategy.ALL) // Cache the image
