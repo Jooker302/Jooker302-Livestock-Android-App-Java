ef91c554cde83c9bf41ba9868a0301aea1112273
Jooker302 <i.omer6637@gmail.com>
2023-06-03 23:14:57 +0500

    Final Touches

--- app/src/main/java/com/example/queenlivestock/PostDetailActivity.java
+++ app/src/main/java/com/example/queenlivestock/PostDetailActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PostDetailActivity extends AppCompatActivity {
     TextView detail_post_title;
     TextView detail_post_description;
     TextView detail_post_price;
     ImageView detail_view_image;
+    Button post_buy;
 
     @Override
     public void onBackPressed() {
@@ -35,6 +41,7 @@ public class PostDetailActivity extends AppCompatActivity {
         detail_post_description = findViewById(R.id.detail_post_description);
         detail_post_title = findViewById(R.id.detail_post_title);
         detail_view_image = findViewById(R.id.detail_post_image);
+        post_buy = findViewById(R.id.post_buy);
 
         detail_post_title.setText(get_post.getTitle());
         detail_post_description.setText(get_post.getDescription());
@@ -50,5 +57,35 @@ public class PostDetailActivity extends AppCompatActivity {
             detail_view_image.setScaleType(ImageView.ScaleType.CENTER_CROP);
         }
 
+        post_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showNotification("Purchased");
+            }
+        });
+
+    }
+
+    private void showNotification(String message) {
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+
+        // Inflate the custom layout
+        View view = getLayoutInflater().inflate(R.layout.custom_register_toast, null);
+        TextView toastText = view.findViewById(R.id.toast_text);
+        toastText.setText(message);
+
+        toast.setView(view);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+
+        // Delay the toast dismissal after 3 seconds
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toast.cancel();
+            }
+        }, 3000); // Adjust the duration as needed (in milliseconds)
     }
 }
