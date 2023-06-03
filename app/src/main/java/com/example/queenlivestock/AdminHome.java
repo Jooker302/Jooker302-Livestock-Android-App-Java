f18737d829affd5af35cec5de344ed6447a5adf9
Jooker302 <i.omer6637@gmail.com>
2023-06-03 20:11:33 +0500

    Fragments Connected By Admin Home

--- app/src/main/java/com/example/queenlivestock/AdminHome.java
+++ app/src/main/java/com/example/queenlivestock/AdminHome.java
@@ -1,22 +1,30 @@
 package com.example.queenlivestock;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.navigation.NavigationView;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class AdminHome extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+
+        fragmentManager = getSupportFragmentManager();
 
         drawerLayout = findViewById(R.id.drawer_layout);
 
@@ -30,9 +38,16 @@ public class AdminHome extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
-                // Handle navigation menu item clicks here
-                // You can identify the selected item by using menuItem.getItemId()
-                drawerLayout.closeDrawers(); // Close the navigation drawer after an item is selected
+                int id = menuItem.getItemId();
+
+                if (id == R.id.admin_menu_users) {
+                    replaceFragment(new AdminUsersFragment());
+                } else if (id == R.id.admin_menu_posts) {
+                    replaceFragment(new AdminPostsFragment());
+                } else if (id == R.id.admin_menu_logout) {
+                    Toast.makeText(AdminHome.this, "Logout", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawers();
                 return true;
             }
         });
@@ -43,6 +58,19 @@ public class AdminHome extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        // Load the initial fragment
+        replaceFragment(new AdminUsersFragment());
+
+        // Preselect the "Users" menu item
+        MenuItem usersMenuItem = navigationView.getMenu().findItem(R.id.admin_menu_users);
+        usersMenuItem.setChecked(true);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.admin_fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
