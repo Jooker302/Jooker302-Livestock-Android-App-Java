33113344d6d907edddece54bea30585e65609c78
Jooker302 <i.omer6637@gmail.com>
2023-06-03 21:15:35 +0500

    Admin Home Done

--- app/src/main/java/com/example/queenlivestock/AdminHome.java
+++ app/src/main/java/com/example/queenlivestock/AdminHome.java
@@ -9,15 +9,22 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.navigation.NavigationView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AdminHome extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private FragmentManager fragmentManager;
+    private static final int TIME_DELAY = 2000;
+    private static long back_pressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +52,10 @@ public class AdminHome extends AppCompatActivity {
                 } else if (id == R.id.admin_menu_posts) {
                     replaceFragment(new AdminPostsFragment());
                 } else if (id == R.id.admin_menu_logout) {
-                    Toast.makeText(AdminHome.this, "Logout", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(AdminHome.this, "Logout", Toast.LENGTH_SHORT).show();
+                    Intent logout =new Intent(getApplicationContext(), Login.class);
+                    showNotification("Logged Out");
+                    startActivity(logout);
                 }
                 drawerLayout.closeDrawers();
                 return true;
@@ -78,7 +88,36 @@ public class AdminHome extends AppCompatActivity {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+//            super.onBackPressed();
+            if (back_pressed + TIME_DELAY > System.currentTimeMillis()) {
+                finishAffinity();
+            } else {
+                Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+            }
+            back_pressed = System.currentTimeMillis();
         }
+        }
+
+    private void showNotification(String message) {
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+
+        // Inflate the custom layout
+        View view = getLayoutInflater().inflate(R.layout.custom_register_toast, null);
+        TextView toastText = view.findViewById(R.id.toast_text);
+        toastText.setText(message);
+
+        toast.setView(view);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+
+        // Delay the toast dismissal after 3 seconds
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toast.cancel();
+            }
+        }, 3000); // Adjust the duration as needed (in milliseconds)
     }
 }
